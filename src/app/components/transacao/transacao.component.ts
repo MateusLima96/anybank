c94c7e57e6881553c4578b12aa46765194eff2eb
Mateus de Araújo Lima <mateuslima020@outlook.com>
2026-02-19 11:27:54 -0300

    feat: add statement component

---
+++ src/app/components/transacao/transacao.component.ts
@@ -0,0 +1,19 @@
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Component, computed, input } from '@angular/core';
+import { TipoTransacao, Transacao } from '../../models/transaction';
+
+@Component({
+  selector: 'app-transacao',
+  imports: [CurrencyPipe, DatePipe],
+  templateUrl: './transacao.component.html',
+  styleUrl: './transacao.component.css'
+})
+export class TransacaoComponent {
+  transacao = input.required<Transacao>()
+  valor = computed(() => {
+    if(this.transacao().tipo === TipoTransacao.SAQUE){
+      return -this.transacao().valor;
+    }
+    return this.transacao().valor;
+  })
+}
